1270703dbda2704b6d66ab169f6bfad6cf0772cb
vicwin13 <vnwimo13@gmail.com>
2022-10-12 23:15:47 +0100

    almost

---
+++ training.js
@@ -0,0 +1,11 @@
+let plus = document.querySelectorAll(".plus");
+let answers = document.querySelectorAll(".answers");
+let reply = [];
+
+// plus.forEach((button) => {
+//   button.addEventListener("click", reply);
+// });
+
+let results = new Map(answers);
+
+console.log(results.values);
